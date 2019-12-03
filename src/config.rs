7ad9c1055056089adcc3e782b8b270841f46711a
Ben Woodcroft <donttrustben@gmail.com>
2019-12-04 06:32:41 +1000

    config: Adjust for cockatoo.

--- src/config.rs
+++ src/config.rs
@@ -13,14 +13,16 @@ pub enum FastaFormat {
 // main configs
 pub const MEM_SIZE: usize = 1;
 pub const MIN_KMERS: usize = 1;
-pub const STRANDED: bool = true;
+pub const STRANDED: bool = false;
 pub const REPORT_ALL_KMER: bool = false;
 pub const READ_COVERAGE_THRESHOLD: usize = 32;
 pub const LEFT_EXTEND_FRACTION: f64 = 0.2;
 
 pub const U32_MAX: usize = u32::max_value() as usize;
 
-pub type KmerType = kmer::Kmer20;
+// Using Kmer24 for cockatoo so tests pass, but no optimisation has been done
+// (computational or scientific).
+pub type KmerType = kmer::Kmer24;
 
 // Transcriptome mappability
 pub const MAPPABILITY_COUNTS_LEN: usize = 11;
